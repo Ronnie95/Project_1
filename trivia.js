605e739ea11071b63fae7ec0f14cb00680016c6b
Ronnie95 <t_daron@yahoo.com>
2023-03-07 21:59:36 -0500

    button functionality

--- trivia.js
+++ trivia.js
@@ -1,16 +1,39 @@
 const firstQ = document.getElementById("red")
 const purple = document.querySelector('.purple');
-const green = document.getElementsByClassName("Green");
-const orange = document.getElementsByClassName("Orange");
+const green = document.querySelector('.green');
+const orange = document.querySelector('.orange');
+const arkansas = document.querySelector('.arkansas');
+const new = document.querySelector('.new');
+const california = document.querySelector('.california');
+const alabama = document.querySelector('.alabama');
+const maine = document.querySelector('.maine');
+const tv = document.querySelector('.tv');
+const Bananas = document.querySelector('.Bananas');
+const clothes = document.querySelector('.clothes');
+const toothpaste = document.querySelector('.toothpaste');
+const Lemons = document.querySelector('.Lemons');
 
 
-firstQ.addEventListener("click", changeRed);
-
+firstQ.addEventListener("click", (event) => changeRed(event));
+firstQ.addEventListener("click", (event) => newHtml(event));
 function changeRed() {
-    firstQ.style.backgroundColor = "red";
+    event.target.style.backgroundColor = "red";
 }
-purple.addEventListener("click", changeHtml)
+function newHtml(event) {
+  event.target.innerHTML = "Incorrect"
+ 
+}
 
-function changeHtml() {
-    purple.innerHTML = "wrong answer tough guy"
+purple.addEventListener("click", (event) => newHtml(event));
+purple.addEventListener("click", (event) => changeRed(event));
+orange.addEventListener("click", (event) => newHtml(event));
+orange.addEventListener("click", (event) => changeRed(event));
+
+green.addEventListener("click", (event) => correctAnswer(event))
+green.addEventListener("click", (event) => correct(event))
+function correctAnswer() {
+    event.target.style.backgroundColor = "green";
+}
+function correct() {
+    event.target.innerHTML = "Correct";
 }
