a46df4b03af9b5e52ddceb482aa61473d433c627
Ronnie95 <t_daron@yahoo.com>
2023-03-07 22:10:18 -0500

    button dom for this page complete

--- trivia.js
+++ trivia.js
@@ -3,7 +3,7 @@ const purple = document.querySelector('.purple');
 const green = document.querySelector('.green');
 const orange = document.querySelector('.orange');
 const arkansas = document.querySelector('.arkansas');
-const new = document.querySelector('.new');
+const newYork = document.querySelector('.new');
 const california = document.querySelector('.california');
 const alabama = document.querySelector('.alabama');
 const maine = document.querySelector('.maine');
@@ -11,11 +11,12 @@ const tv = document.querySelector('.tv');
 const Bananas = document.querySelector('.Bananas');
 const clothes = document.querySelector('.clothes');
 const toothpaste = document.querySelector('.toothpaste');
-const Lemons = document.querySelector('.Lemons');
+const Lemons = document.querySelector('.lemons');
 
 
 firstQ.addEventListener("click", (event) => changeRed(event));
 firstQ.addEventListener("click", (event) => newHtml(event));
+
 function changeRed() {
     event.target.style.backgroundColor = "red";
 }
@@ -26,14 +27,43 @@ function newHtml(event) {
 
 purple.addEventListener("click", (event) => newHtml(event));
 purple.addEventListener("click", (event) => changeRed(event));
+
 orange.addEventListener("click", (event) => newHtml(event));
 orange.addEventListener("click", (event) => changeRed(event));
 
 green.addEventListener("click", (event) => correctAnswer(event))
 green.addEventListener("click", (event) => correct(event))
+
 function correctAnswer() {
     event.target.style.backgroundColor = "green";
 }
 function correct() {
     event.target.innerHTML = "Correct";
 }
+
+arkansas.addEventListener("click", (event) => newHtml(event));
+arkansas.addEventListener("click", (event) => changeRed(event));
+
+california.addEventListener("click", (event) => newHtml(event));
+california.addEventListener("click", (event) => changeRed(event));
+
+maine.addEventListener("click", (event) => newHtml(event));
+maine.addEventListener("click", (event) => changeRed(event));
+
+alabama.addEventListener("click", (event) => correctAnswer(event))
+alabama.addEventListener("click", (event) => correct(event))
+
+Bananas.addEventListener("click", (event) => correctAnswer(event))
+Bananas.addEventListener("click", (event) => correct(event))
+tv.addEventListener("click", (event) => newHtml(event));
+tv.addEventListener("click", (event) => changeRed(event));
+clothes.addEventListener("click", (event) => newHtml(event));
+clothes.addEventListener("click", (event) => changeRed(event));
+toothpaste.addEventListener("click", (event) => newHtml(event));
+toothpaste.addEventListener("click", (event) => changeRed(event));
+Lemons.addEventListener("click", (event) => newHtml(event));
+Lemons.addEventListener("click", (event) => changeRed(event));
+
+
+
+
