944bbb8c322b13701ac35fe532d4df9d8997c7d9
Ronnie95 <t_daron@yahoo.com>
2023-03-11 07:36:11 -0500

    new updates

--- easy.js
+++ easy.js
@@ -13,6 +13,12 @@ const adam = document.querySelector('.adam');
 const leo = document.querySelector('.leo');
 const denzel = document.querySelector('.denzel');
 const idris = document.querySelector('.idris');
+const begin = document.querySelector('.begin');
+const q1 = document.querySelector('.q1');
+const nxtb = document.querySelector('.nxtb');
+const q2 = document.querySelector('.q2');
+const nxtc = document.querySelector('.nxtc');
+const q3 = document.querySelector('.q3');
 
 yangR.addEventListener("click", (event) => changeRed(event));
 amazon.addEventListener("click", (event) => changeRed(event));
@@ -42,3 +48,51 @@ adam.addEventListener("click", (event) => changeRed(event));
 idris.addEventListener("click", (event) => changeRed(event));
 denzel.addEventListener("click", (event) => changeRed(event));
 leo.addEventListener("click", (event) => correctAnswer(event))
+
+
+begin.addEventListener("click", () => {
+    if(q1.style.display == "none"){
+        q1.style.display = "block";
+    }
+    else{
+        q1.style.display = "none";
+    }
+})
+
+
+nileR.addEventListener("click", () => {
+    if(nxtb.style.display == "none"){
+        nxtb.style.display = "block";
+    }
+    else{
+        nxtb.style.display = "none";
+    }
+})
+
+nxtb.addEventListener("click", () => {
+    if(q2.style.display == "none"){
+        q2.style.display = "block";
+    }
+    else{
+        q2.style.display = "none";
+    }
+})
+
+fut.addEventListener("click", () => {
+    if(nxtc.style.display == "none"){
+        nxtc.style.display = "block";
+    }
+    else{
+        nxtc.style.display = "none";
+    }
+})
+
+
+nxtc.addEventListener("click", () => {
+    if(q3.style.display == "none"){
+        q3.style.display = "block";
+    }
+    else{
+        q3.style.display = "none";
+    }
+})
--- int.js
+++ int.js
@@ -10,6 +10,12 @@ const year = document.querySelector('.year');
 const year1 = document.querySelector('.year1');
 const year2 = document.querySelector('.year2');
 const year3 = document.querySelector('.year3');
+const begin = document.querySelector('.begin');
+const q1 = document.querySelector('.q1');
+const n = document.querySelector('.n');
+const q2 = document.querySelector('.q2');
+const n1 = document.querySelector('.n1');
+const q3 = document.querySelector('.q3');
 
 
 
@@ -40,3 +46,50 @@ year.addEventListener("click", (event) => changeRed(event));
 year2.addEventListener("click", (event) => changeRed(event));
 year3.addEventListener("click", (event) => changeRed(event));
 year1.addEventListener("click", (event) => correctAnswer(event));
+
+begin.addEventListener("click", () => {
+    if(q1.style.display == "none"){
+        q1.style.display = "block";
+    }
+    else{
+        q1.style.display = "none";
+    }
+})
+
+
+dogs.addEventListener("click", () => {
+    if(n.style.display == "none"){
+        n.style.display = "block";
+    }
+    else{
+        n.style.display = "none";
+    }
+})
+
+n.addEventListener("click", () => {
+    if(q2.style.display == "none"){
+        q2.style.display = "block";
+    }
+    else{
+        q2.style.display = "none";
+    }
+})
+
+gin.addEventListener("click", () => {
+    if(n1.style.display == "none"){
+        n1.style.display = "block";
+    }
+    else{
+        n1.style.display = "none";
+    }
+})
+
+
+n1.addEventListener("click", () => {
+    if(q3.style.display == "none"){
+        q3.style.display = "block";
+    }
+    else{
+        q3.style.display = "none";
+    }
+})
